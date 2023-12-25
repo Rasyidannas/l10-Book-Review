dc0121a756b367c5b013407e23d849a9e58af8b4
rasyid annas <rasshit.dsgn@gmail.com>
2023-12-25 14:14:11 +0700

    021. Rate Limiting.mp4

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,6 +28,11 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+        //this for limit create 3 reviews per hour
+        RateLimiter::for('reviews', function (Request $request) {
+            return Limit::perHour(3)->by($request->user()?->id ?: $request->ip());
+        });
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
